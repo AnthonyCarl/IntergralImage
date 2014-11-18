e59747330269c1bfe8b8c80524bd9e68c7b85dda
anthonycarl <smeat32@yahoo.com>
2014-11-18 14:46:05 -0500

    moved comment.

--- IntegralImage/DoublingRatio.cs
+++ IntegralImage/DoublingRatio.cs
@@ -4,6 +4,7 @@ namespace IntegralImage
 {
     public static class DoublingRatio
     {
+        //Because I want to still be able to use my machine if this goes south
         private const int MaxN = 1024*1024;
         public static void Run(int startingN, Func<int, TimeSpan> timeTrial)
         {
@@ -21,7 +22,6 @@ namespace IntegralImage
                 Console.WriteLine("{0,6} {1,9}ms {2,5}", currentN, currentMs, currentRatio);
                 previousMs = currentMs;
                 
-                //16384 because I want to still be able to use my machine if this goes south
                 if (currentN >= MaxN)
                 {
                     Console.WriteLine("Stopping the madness before all memory are belong to us.");
